ba9b07f0b1ded398d39f43ade440012322c8854f
Zeisha <poonamyadavbpl@gmail.com>
2020-08-22 14:13:36 -0500

    Logout changes done

--- __main__.py
+++ __main__.py
@@ -28,7 +28,7 @@ def login():
         user = request.form.get('user', '')
         pawd = request.form.get('pawd', '')
         if USERS.get(user) == pawd:
-            resp = make_response(redirect('/logged_in'))
+            resp = make_response(redirect('/hello'))
             resp.set_cookie('user', user)
             resp.set_cookie('pawd', pawd)
             return resp
@@ -38,11 +38,12 @@ def login():
         return render_template('login.html', user='', pawd='')
 
 
-@app.route('/logged_in')
-def logged_in():
-    user = request.cookies.get('user', '')
-    pawd = request.cookies.get('pawd', '')
-    return render_template('logged_in.html', user=user, pawd=pawd)
+@app.route('/logout', methods=['POST', 'GET'])
+def logout():
+        resp = make_response(render_template('logout.html'))
+        resp.set_cookie('user', '', expires=0)
+        resp.set_cookie('pawd', '', expires=0)
+        return resp
 
 
 if __name__ == '__main__':
